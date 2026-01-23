048eebaea1d93f3fa1d951f813c8e17a81cc88b1
NguyenXuanAu <aunxpk04299@gmail.com>
2026-01-23 22:38:25 +0700

    Module quản lý khách hàng

--- QuanLyQuanCafeAnYenBackend/Program.cs
+++ QuanLyQuanCafeAnYenBackend/Program.cs
@@ -12,7 +12,13 @@ builder.Services.AddSwaggerGen();
 
 //Kết nối với database SQL Server
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); builder.Services.AddDbContext<QuanLyQuanCafeDbContext>(options => options.UseSqlServer(connectionString));
-
+//Cấp quyền API
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowVueApp",
+        policy => policy.AllowAnyOrigin() // Thử cho phép tất cả để test trước
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowVueApp");
 
 app.UseHttpsRedirection();
 
